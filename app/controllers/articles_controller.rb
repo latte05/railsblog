107808eca49343b745db873118e88a34daaad76d
rails course <camaroa@hotmail.com>
2017-02-02 05:49:46 +0000

    create redundancy for productions

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
 
+ before_action :set_article, only: [:edit, :update, :show, :destroy]
+
   def index
     @articles = Article.all
   end
@@ -9,7 +11,6 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
@@ -25,31 +26,30 @@ class ArticlesController < ApplicationController
   end
 
  def show
-   @article = Article.find(params[:id])
  end
 
  def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article is succesfully deleted"
     redirect_to articles_path
  end
 
  def update
-   @article = Article.find(params[:id])
    if @article.update(article_params)
      flash[:notice] = "Article was successfully updated"
     redirect_to article_path(@article)
    else
       render :edit
-   end
+  end
  end
 
 
+private
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-  private
     def article_params
       params.require(:article).permit(:title, :description)
     end
-
 end
